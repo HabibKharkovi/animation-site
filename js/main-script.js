79d39110878f4a70c8778d86c164ea2fd7d1435b
sabbar <Sabbar.iplex@gmail.com>
2020-05-07 10:56:09 -0700

    landing page changes

--- js/main-script.js
+++ js/main-script.js
@@ -1,71 +1,64 @@
 gsap.registerPlugin(Text);
-const tl = gsap.timeline();
+landingPageAnimation()
 
-$('.blur').on('wheel', function(e){
-    tl.to('.blur', {filter: "blur(0px)", duration: 1})
-    $('body').removeClass('fixed');
-}, mainPageAnimation())
- 
 
-function mainPageAnimation(){
+function landingPageAnimation(){
 
-    const Split_text = new Text('.sm-text, .lg-text', {type:"words, chars", wordDelimiter: " " });
-    const started_link =  new Text('.started-link', {type:"words, chars" });
-    const chars = Split_text.chars;
-    const chars1 = started_link.chars;
+    const tl1 = gsap.timeline();
+
+    $('.blur').on('wheel', function(e){
+        tl1.to('.blur', {filter: "blur(0px)", duration: 1})
+        $('body').removeClass('fixed');
+    })
+
+    const tl2 = gsap.timeline();
 
     let i = 0;
+
     $('.main-page').on('drag, click', function(){
 
-        if(tl.isActive()){
+        if(tl2.isActive()){
             return false
         }
 
-        if (i === 1){
-            tl.to(Split_text.chars, { duration: 0.8, opacity: 0, stagger: 0.04
-            }, 3);
-            tl.to('.product-img', {x: "-50vw", duration: 1})
-            tl.to('.box', {right: "-9%", duration: 1.4}, "-=1.4")
-            tl.from(chars1, {duration: 0.8, opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50",  ease:"back", stagger: 0.01}, "+=0");
+        const mainHeading = new Text('.sm-text, .lg-text', {type:"words, chars", wordDelimiter: " " });
+        const started_link =  new Text('.started-link-text', {type:"words, chars" });
+
+        function AllDone(){
+            tl2.reversed()
         }
 
-
+    
         if(i === 0){
             i = 1;
             
             gsap.set(".sm-text, .lg-text", {perspective: 400});
-            tl.to('.product-img', {x: "-18vw", duration: 1})
-            tl.to(".sm-text, .lg-text", {visibility: "visible"});
-            tl.from(chars, {duration: 0.8, opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50",  ease:"back", stagger: 0.01}, "+=0");
+            tl2.to('.product-img', {x: "-18vw", duration: 1})
+            tl2.to(".sm-text, .lg-text", {visibility: "visible"});
+            tl2.from(mainHeading.chars, {duration: 1, opacity:0, scale:0, y:80, x: 80, rotationX:100, transformOrigin:"0% 50% -50",  ease:"back", stagger: 0.13}, "+=0", AllDone);
+
+            return false;
         } 
+
+       
+
+        if (i === 1){
+            i = 2;
+            tl2.to(mainHeading.chars, { duration: 0.8, opacity: 0, stagger: 0.1}, AllDone);
+            tl2.to('.product-img', {x: "-50vw", duration: 1})
+            tl2.to('.box', {right: "-9%", duration: 1.4}, "-=1.4")
+            gsap.set('.cls-1', {perspective: 400});
+            tl2.from('.cls-1', {duration: 1, opacity:0, scale:0, y:80, rotationX:180, transformOrigin:"0% 50% -50",  ease:"back", stagger: 0.13})
+            tl2.from('.cls-box', {duration: 2, x: 200, autoAlpha: 0, ease: "elastic", stagger: 0.3})
+            tl2.from(started_link.chars, {duration: 1, opacity:0, scale:0, y:80, x: 80, rotationX:100, transformOrigin:"0% 50% -50",  ease:"back", stagger: 0.13}, "+=0", AllDone);
+
+            return false;
+        }
         
     })
 }
 
-// (function () {
-//     var $frame  = $('.slider-wrapper');
-//     var $wrap   = $frame.parent();
 
-//     // Call Sly on frame
-//     $frame.sly({
-//         itemNav: 'basic',
-//         smart: 1,
-//         activateOn: 'click',
-//         mouseDragging: 1,
-//         touchDragging: 1,
-//         releaseSwing: 1,
-//         startAt: 0,
-//         activatePageOn: 'click',
-//         speed: 300,
-//         elasticBounds: 1,
-//         dragHandle: 1,
-//         dynamicHandle: 1,
-//         clickBar: 1,
-
-//         prevPage: $wrap.find('.prev'),
-// 		nextPage: $wrap.find('.next')
-//     });
-// }());
 
 
 
